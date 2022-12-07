63c963a2d7c1b3c8ce1c0ccb0c1e688fb1c6b8a9
qq84808045 <2634917964@qq.com>
2022-12-07 18:33:17 +0800

    style:删除main.js中的打印

--- src/main.js
+++ src/main.js
@@ -19,12 +19,9 @@ global.onlineUsers = new Map();
 io.on("connection",(socket)=>{
   global.chatSocket = socket;
   socket.on("add-user",(userId)=>{
-    console.log('userId:',userId);
     onlineUsers.set(userId,socket)
-    // onlineUsers.set(userId,userId)
   })
   socket.on("send-msg", (data) => {
-    console.log('data',data);
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
       // socket.to(sendUserSocket).emit("msg-recieve", data.msg);
